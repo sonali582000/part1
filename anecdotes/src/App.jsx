ff63ba00cce1d68f7625f6d48b8f9a753e065e9a
sonali582000 <rathodsonali393@gmail.com>
2024-03-01 11:58:24 +0100

    Done

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,9 +1,49 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState([1, 4, 6, 3, 0, 0, 0, 0]);
+
+  // const points = [1, 4, 6, 3, 0, 0, 0, 0];
+
+  const randomNumber = () => {
+    const generateRandomNumber = Math.floor(Math.random() * anecdotes.length);
+    setSelected(generateRandomNumber);
+  };
+
+  const handleVote = () => {
+    const copy = [...votes];
+    // increment the value in position 2 by one
+    copy[selected] += 1;
+    setVotes(copy);
+    console.log(copy);
+  };
+
+  const maxIndexValue = votes.indexOf(Math.max(...votes));
+
   return (
-    <>
-      <h1>ajg</h1>
-    </>
+    <div>
+      <h1>Anecdotes of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={randomNumber}>next anecdotes</button>
+
+      <h1>Anecdotes with the most vote</h1>
+      <p>{anecdotes[maxIndexValue]}</p>
+    </div>
   );
-}
+};
 
 export default App;
